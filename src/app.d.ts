149632289f229f4c3b6453edd9d7f9d4c487e2c7
Josef Forkman <strand.vatten@outlook.com>
2023-10-20 12:28:03 +0200

    fix typs

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,7 +1,7 @@
 // src/app.d.ts
 
 import { SupabaseClient, Session } from '@supabase/supabase-js'
-import { Database } from './DatabaseDefinitions'
+import { Database } from './supabase'
 
 declare global {
   namespace App {
