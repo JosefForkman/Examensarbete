88daad5d3c87168c76a495caa84c796a5cf3f6ae
Styrbjörn Nordberg <styris.n@gmail.com>
2023-10-24 13:42:28 +0200

    started on styling login and signup

---
+++ src/routes/login&signUp/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	// fill if needs
+</script>
+
+<main>
+	<div class="linkContainer">
+		<a class="linkBtn">Login</a>
+		<a class="linkBtn">Sign Up</a>
+	</div>
+</main>
+
+<style>
+	main {
+		width: 100%;
+		height: 100vh;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	.linkContainer {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 18px;
+	}
+	.linkBtn {
+		padding: 17px 60px;
+		border-radius: 100%;
+		cursor: pointer;
+	}
+</style>
