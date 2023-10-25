9412831b86b2c4950f27cc89ae369f4cc1e2fb71
Styrbjörn Nordberg <styris.n@gmail.com>
2023-10-25 10:15:15 +0200

    added protected routes

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,6 @@
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit';
-import type { Handle } from '@sveltejs/kit';
+import { type Handle, redirect, error } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.supabase = createSupabaseServerClient({
@@ -16,7 +16,14 @@ export const handle: Handle = async ({ event, resolve }) => {
 		return session;
 	};
 
-  
+	// protect requests to all routes that start with /protected-routes
+	if (event.url.pathname.startsWith('/protected-routes')) {
+		const session = await event.locals.getSession();
+		if (!session) {
+			// the user is not signed in
+			throw redirect(303, '/login&signUp');
+		}
+	}
 
 	return resolve(event, {
 		filterSerializedResponseHeaders(name) {
