40b3b257d732a2e7ee8ee749a0813313442e795d
JosefForkman <strand.vatten@outlook.com>
2023-10-31 12:48:52 +0100

    the beginning of the design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,72 @@
-<script lang="ts">
-	import type { PageData } from './$types';
-
-	export let data: PageData;
-	//@ts-ignore
-	const { Products } = data;
+<script>
+	import src from '$lib/assets/pexels-photo-4016579 1.png';
 </script>
 
 <main>
-	<h1>Welcome to SvelteKit</h1>
-	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-	{#if Products}
-		{#each Products as Product}
-			<p>{Product.description}</p>
-		{/each}
-	{:else}
-		<p>LMAO</p>
-	{/if}
+	<section>
+		<h1>Din Port till Godisparadiset!</h1>
+		<p>
+			ChokladFrossa är din digitala godisbutik med ett varierat sortiment av läckra och frestande
+			godisar från hela världen. Beställ dina favoriter enkelt online med snabb leverans och säker
+			betalning för att göra din dag lite sötare.
+		</p>
+		<a class="btn" href="/Godis">Products</a>
+		<img {src} alt="" />
+	</section>
 </main>
+
+<style>
+	main {
+		margin-inline: 1rem;
+	}
+	section {
+		display: grid;
+		grid-template-columns: repeat(1, 1fr);
+		grid-template-rows: auto auto 1fr;
+		align-items: start;
+		gap: 1rem;
+	}
+	section h1 {
+		color: var(--Primary);
+	}
+	img {
+		grid-row: 1;
+		width: 100%;
+		border-radius: 0.5rem;
+	}
+
+	@media (width >= 768px) {
+		section {
+			grid-template-columns: 2fr auto;
+			gap: 1rem 1.5rem;
+		}
+
+		img {
+			height: 35vh;
+			object-fit: cover;
+		}
+		img, h1 {
+			grid-column: span 2;
+		}
+	}
+
+	@media (width >= 1024px) {
+		main {
+			margin-inline: 3rem;
+		}
+		section {
+			grid-template-columns: repeat(2, 1fr);
+			justify-items: start;
+			gap: 1rem 3rem;
+		}
+		img {
+			grid-column: 2;
+			grid-row: 1 / span 3;
+			height: auto;
+		}
+
+		h1 {
+			grid-column: 1;
+		}
+	}
+</style>
