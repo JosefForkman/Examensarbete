0cd8313ff6e9cc42a5022ac30052fa78a0b2adde
JosefForkman <strand.vatten@outlook.com>
2023-11-12 09:14:59 +0100

    fix zod to only use one

--- src/routes/protected-routes/settings/+page.server.ts
+++ src/routes/protected-routes/settings/+page.server.ts
@@ -1,105 +1,117 @@
-import { fail, redirect } from '@sveltejs/kit';
+import { fail, json, redirect } from '@sveltejs/kit';
 import { superValidate } from 'sveltekit-superforms/server';
 import { z } from 'zod';
 import { stripe } from '$lib/server/stripe.js';
 
-const formSchema = z.object({
-	name: z.string().nullish(),
-	phone: z.coerce.string().nullish(),
-	city: z.string().nullish(),
-	line1: z.string().nullish(),
-	line2: z.string().nullish(),
-	postal_code: z.string().nullish()
+/* Todo: move zode schema and types to lib folder */
+const addressSchema = z.object({
+	city: z.coerce.string().optional(),
+	line1: z.coerce.string().optional(),
+	line2: z.coerce.string().optional(),
+	postal_code: z.string().optional()
 });
 
+type addressSchemaType = z.infer<typeof addressSchema>;
+
+const profileSchema = z.object({
+	name: z.string().optional(),
+	phone: z.string().optional(),
+	address: addressSchema.nullable()
+});
+
+type profileSchemaType = z.infer<typeof profileSchema>;
+
+
 export const load = async ({ locals: { supabase } }) => {
-	const { data: profiles, error } = await supabase.from('Profiles').select('*');
+	const formCustomer: profileSchemaType = {
+		name: undefined,
+		phone: undefined,
+		address: {
+			city: undefined,
+			line1: undefined,
+			line2: undefined,
+			postal_code: undefined
+		}
+	};
+	
+	const address: addressSchemaType | null = {
+		city: undefined,
+		line1: undefined,
+		line2: undefined,
+		postal_code: undefined
+	};
+	const { data: profiles } = await supabase.from('Profiles').select('*').single();
 
+	/* Check if profiles exist */
 	if (!profiles) {
-		return;
+		const form = await superValidate(formCustomer, profileSchema);
+		return { form };
 	}
-	const customer = await stripe.customers.retrieve(profiles[0].stripe_customer_id);
 
-	//@ts-ignore
-	if (customer.address !== null) {
-		const profileSchema = z.object({
-			name: z.string().nullish(),
-			phone: z.coerce.string().nullish(),
-			address: z.object({
-				city: z.string().nullish(),
-				line1: z.string().nullish(),
-				line2: z.string().nullish(),
-				postal_code: z.string().nullish()
-			})
-		});
+	/* Check if stripe customer is a valid object and format */
+	const customer = profileSchema.safeParse(
+		await stripe.customers.retrieve(profiles.stripe_customer_id)
+	);
 
-		const parsedCustomer = profileSchema.parse(customer);
-
-		const formCustomer = {
-			name: parsedCustomer.name,
-			phone: parsedCustomer.phone,
-			city: parsedCustomer.address.city,
-			line1: parsedCustomer.address.line1,
-			line2: parsedCustomer.address.line2,
-			postal_code: parsedCustomer.address.postal_code
-		};
-
-		const form = await superValidate(formCustomer, formSchema);
-
-		return { formCustomer, form };
-	} else {
-		const parsedCustomer = formSchema.parse(customer);
-		console.log(parsedCustomer);
-
-		const form = await superValidate(parsedCustomer, formSchema);
-
-		return { parsedCustomer, form };
+	if (!customer.success) {
+		const form = await superValidate(formCustomer, profileSchema);
+		return { form };
 	}
+
+	/* Add adress object if adress == null */
+	if (!customer.data.address) {
+		const form = await superValidate(
+			{
+				...customer.data,
+				address
+			},
+			profileSchema
+		);
+
+		return { form };
+	}
+
+	const form = await superValidate(customer.data, profileSchema);
+	return { form };
 };
 
-function updateUser(customer: any, formData: any, customerId: any) {
-	const stripeDataSchema = z.object({
-		name: z.coerce.string().optional(),
-		phone: z.coerce.string().optional(),
-		city: z.coerce.string().optional(),
-		line1: z.coerce.string().optional(),
-		line2: z.coerce.string().optional(),
-		postal_code: z.coerce.string().optional()
-	});
-	const newUserData = formData;
-	const parsedUserData = stripeDataSchema.parse(newUserData);
+function updateUser(formData: profileSchemaType, customerId: string) {
+	/* If adress is not enter make adress == null */
+	if (formData.address) {
+		const { city, line1, line2, postal_code } = formData.address;
 
-	return stripe.customers.update(customerId, {
-		name: parsedUserData.name,
-		phone: parsedUserData.phone,
-		address: {
-			city: parsedUserData.city,
-			line1: parsedUserData.line1,
-			line2: parsedUserData.line2,
-			postal_code: parsedUserData.postal_code
+		if (!city && !line1 && !line2 && !postal_code) {
+			return stripe.customers.update(customerId, { ...formData, address: null });
 		}
-	});
+	}
+
+	const parsedStripeCustomerData = profileSchema.parse(formData);
+	return stripe.customers.update(customerId, parsedStripeCustomerData);
 }
 
 export const actions = {
 	default: async ({ request, locals: { supabase } }) => {
-		const { data: profiles, error } = await supabase.from('Profiles').select('*');
-
-		if (!profiles) {
-			return;
-		}
-		const customer = await stripe.customers.retrieve(profiles[0].stripe_customer_id);
-		const form = await superValidate(request, formSchema);
-		const { data: profile, error: profileError } = await supabase.from('Profiles').select('*');
-		if (profileError) {
-			return fail(400, { form, profileError });
-		}
+		const form = await superValidate(request, profileSchema);
 
 		if (!form.valid) {
 			return fail(400, { form });
 		}
 
-		const updatedCustomer = updateUser(customer, form.data, profile[0].stripe_customer_id);
+		const { data: profile, error: profileError } = await supabase
+			.from('Profiles')
+			.select('stripe_customer_id')
+			.single();
+
+		if (!profile) {
+			return json('Kunde inte hitta profile', { status: 404 });
+		}
+
+		if (profileError) {
+			return fail(400, { form, profileError });
+		}
+
+		const customer = await stripe.customers.retrieve(profile.stripe_customer_id);
+		updateUser(form.data, customer.id);
 
 		throw redirect(303, '/protected-routes/settings');
 	}
