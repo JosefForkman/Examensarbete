469d4eb63b102512ac8538fda3dd918dbbac4fc6
Styrbjörn Nordberg <styris.n@gmail.com>
2023-11-22 13:45:23 +0100

    minor changes to fetches and more clean up

--- src/routes/protected-routes/orders/[slug]/+page.server.ts
+++ src/routes/protected-routes/orders/[slug]/+page.server.ts
@@ -6,37 +6,26 @@ export const load: PageServerLoad = async ({ params, locals: { supabase } }) => 
 		.from('Orders')
 		.select(
 			'id, order_date, delivery_date, stripe_payment_intent_id,stripe_customer_id, Order_items (id, product_id, quantity, Products (*))'
-		).match({id: params.slug});
+		)
+		.match({ id: params.slug });
+
+	const { data: profiles } = await supabase.from('Profiles').select('stripe_customer_id').single();
+
+	if (!profiles) {
+		return;
+	}
+	if (!orders) {
+		return;
+	}
 
 	if (orders?.length !== 0) {
+
+		if (orders[0].stripe_customer_id != profiles.stripe_customer_id) {
+			throw error(403, 'forbiden')
+		}
+
 		return { orders };
 	}
+
 	throw error(404, 'Order no found');
 };
-// import type { PageServerLoad } from './$types';
-
-// export const load: PageServerLoad = async ({ locals: { supabase } }) => {
-// 	const { data: profiles, error: profilesError } = await supabase.from('Profiles').select('*');
-
-// 	if (profilesError) {
-// 		console.log(profilesError);
-// 	}
-
-// 	if (!profiles) {
-// 		return;
-// 	}
-
-// 	const { data: orders, error: ordersError } = await supabase
-// 		.from('Orders')
-// 		.select(
-// 			'id, order_date, delivery_date, stripe_payment_intent_id,stripe_customer_id, Order_items (id, product_id, quantity, Products (*))'
-// 		)
-// 		.eq('stripe_customer_id', profiles[0].stripe_customer_id);
-// 	if (ordersError) {
-// 		console.log(ordersError);
-// 	}
-
-// 	if (orders?.length !== 0) {
-// 		return { orders };
-// 	}
-// };
