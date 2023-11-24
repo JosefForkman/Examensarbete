c3747289b3ed79f9c070dd6b99727d9ddf1d814c
Styrbjörn Nordberg <styris.n@gmail.com>
2023-11-24 10:39:25 +0100

    pain fix

---
+++ src/routes/StripeElements/cancel/+page.svelte
@@ -0,0 +1,6 @@
+<h1>Du har <span>misslyckade</span> att betala</h1>
+<style>
+	span {
+		color: red;
+	}
+</style>
---
+++ src/routes/StripeElements/success/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { cartStore } from '$lib/cartStore/cart';
+	import { onMount } from 'svelte';
+	const { Clear } = cartStore();
+	onMount(() => {
+		Clear()
+	})
+</script>
+<h1>Du har lyckas att betala</h1>
